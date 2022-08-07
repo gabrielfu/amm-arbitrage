1db2ca1f6c052af9cd7691aa8b5d02df519c2168
Gabriel Fu <hfu.gabriel@gmail.com>
2022-08-07 12:43:47 +0800

    add quote tokens

--- src/tokens.ts
+++ src/tokens.ts
@@ -19,14 +19,14 @@ const baseTokens: Token[] = [
 ];
 
 const quoteTokens: Token[] = [
-    // { symbol: 'WETH', address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' },
-    // { symbol: 'DAI', address: '0x6b175474e89094c44da98b954eedeac495271d0f' },
-    // { symbol: 'USDC', address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' },
-    // { symbol: 'USDT', address: '0xdac17f958d2ee523a2206206994597c13d831ec7' },
-    // { symbol: 'LINK', address: '0x514910771af9ca656af840dff83e8264ecf986ca' },
-    // { symbol: 'ZRX', address: '0xe41d2489571d322189246dafa5ebde1f4699f498' },
-    // { symbol: 'SNX', address: '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f' },
-    // { symbol: 'WBTC', address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' },
+    { symbol: 'WETH', address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' },
+    { symbol: 'DAI', address: '0x6b175474e89094c44da98b954eedeac495271d0f' },
+    { symbol: 'USDC', address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' },
+    { symbol: 'USDT', address: '0xdac17f958d2ee523a2206206994597c13d831ec7' },
+    { symbol: 'LINK', address: '0x514910771af9ca656af840dff83e8264ecf986ca' },
+    { symbol: 'ZRX', address: '0xe41d2489571d322189246dafa5ebde1f4699f498' },
+    { symbol: 'SNX', address: '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f' },
+    { symbol: 'WBTC', address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' },
     { symbol: 'AAVE', address: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9' },
     { symbol: 'UNI', address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984' },
 ];
